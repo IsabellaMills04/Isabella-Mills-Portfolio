3541a9d0412d1cb689f90103f685c612d7b50fc4
Isabella Mills <isabellamills04@gmail.com>
2026-02-18 19:00:55 -0600

    more work

---
+++ src/Theme.js
@@ -0,0 +1,16 @@
+const Theme = {
+    colors: {
+        pink1: '#f5c2b1',
+        pink2: '#f5a791',
+        pink3: '#f39c6f',
+        yellow1: '#f4b744',
+        yellow2: '#f8a82d',
+        green1: '#ddeede',
+        green2: '#cdd5ae',
+        green3: '#bdcd74',
+        blue1: '#d0e4e3',
+        purple1: '#e7e6f4',
+        purple2: '#d9cbe5',
+        purple3: '#cbbecf',
+    }
+}
